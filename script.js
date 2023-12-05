d424b9a520978b488a7a71a11eccc6869cae389e
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-12-04 23:39:12 -0500

    worked on more formatting around the current weather and forecasting

--- script.js
+++ script.js
@@ -1,22 +1,44 @@
-// currenWeatherListEL used for appending later into the unordered list
+// selectedCity is what will be in the title of the city that was selected. On initial page load will have date.
+let selectedCityTitle = document.querySelector("#currentSelectedCity")
+selectedCityTitle.textContent = ("Today's Date: ") + dayjs().format("M/d/YYYY") 
+
+// *CURRENT WEATHER SECTION
+// currenWeatherListEL used for appending later into the unordered list for current weather info
 const currentWeatherListEl = document.querySelector("#currentWeatherList")
 // li's will be used to append onto currentweather list for info on current weather
-let ListEl = document.createElement("li")
-// selectedCity is what will be in the title of the city that was selected
-let selectedCityTitle = document.querySelector("#currentSelectedCity")
+let currentTempEL = document.createElement("li")
+let currentWindEl = document.createElement("li")
+let currentHumidityEl = document.createElement("li")
+
+let idk1 = currentWeatherListEl.append(currentTempEL)
+let idk2 = currentWeatherListEl.append(currentWindEl)
+let idk3 = currentWeatherListEl.append(currentHumidityEl)
+
+currentTempEL.textContent = "Temp: "
+currentWindEl.textContent = "Wind: "
+currentHumidityEl.textContent = "Humidity: "
+
+// *FIVE DAY SECTION
 //5-Day Forecast Header that will be used as the parent to the li children weather info
-let fiveDayEl = document.querySelector("#fiveDay")
-    fiveDayEl.textContent = "5-Day Forecast:"
+let fiveDayHeaderEl = document.querySelector("#fiveDayHeader")
+    fiveDayHeaderEl.textContent = "5-Day Forecast:"
 
-// li's needed for both current weather and five day forecast
-//Include the date before a city is selected, but update text content after a city is selected
-    selectedCityTitle.textContent = "City selected and Date"
-    currentTempEL = currentWeatherListEl.append(ListEl.textContent = " test ")
-    currentWindEl = currentWeatherListEl.append(ListEl.textContent = " test ")
-    currentHumidityEl = currentWeatherListEl.append(ListEl.textContent = " test ")
+let fiveDayListEl = document.querySelector("#fiveDayList")
 
-// li's needed for 5-Day Forecast
+let nextDayDate = document.createElement("li")
+let nextDayTemp = document.createElement("li")
+let nextDayWind = document.createElement("li")
+let nextDayHumidity = document.createElement("li")
+// Figure out how to loop this so each day doesn't need variables
+nextDay1 = fiveDayListEl.append(nextDayDate)
+nextDay2 = fiveDayListEl.append(nextDayTemp)
+nextDay3 = fiveDayListEl.append(nextDayWind)
+nextDay4 = fiveDayListEl.append(nextDayHumidity)
 
+nextDayDate.textContent = 
+nextDayTemp.textContent = 
+nextDayWind.textContent =
+nextDayHumidity.textContent =
 
 
 const apiKey = "64205da5f805a7fb413caa37e89fd954"
