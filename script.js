6dfd43ba10afc70c723be59d2fbca3dc6e94b04b
Tyler-Cav <Tyler.Cavanaugh24@gmail.com>
2023-12-05 01:14:25 -0500

    More formatting, was able to correctly update data for current day based on city input

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // selectedCity is what will be in the title of the city that was selected. On initial page load will have date.
 let selectedCityTitle = document.querySelector("#currentSelectedCity")
-selectedCityTitle.textContent = ("Today's Date: ") + dayjs().format("M/d/YYYY") 
+selectedCityTitle.textContent = ("Today's Date: ") + dayjs().format('M/D/YYYY') 
+
+let dateTest = dayjs(1).format('D')
+console.log(dateTest)
 
 // *CURRENT WEATHER SECTION
 // currenWeatherListEL used for appending later into the unordered list for current weather info
@@ -10,9 +13,9 @@ let currentTempEL = document.createElement("li")
 let currentWindEl = document.createElement("li")
 let currentHumidityEl = document.createElement("li")
 
-let idk1 = currentWeatherListEl.append(currentTempEL)
-let idk2 = currentWeatherListEl.append(currentWindEl)
-let idk3 = currentWeatherListEl.append(currentHumidityEl)
+currentWeatherListEl.append(currentTempEL)
+currentWeatherListEl.append(currentWindEl)
+currentWeatherListEl.append(currentHumidityEl)
 
 currentTempEL.textContent = "Temp: "
 currentWindEl.textContent = "Wind: "
@@ -23,22 +26,23 @@ currentHumidityEl.textContent = "Humidity: "
 let fiveDayHeaderEl = document.querySelector("#fiveDayHeader")
     fiveDayHeaderEl.textContent = "5-Day Forecast:"
 
-let fiveDayListEl = document.querySelector("#fiveDayList")
+let dayOneIDEl = document.querySelector("#dayOneID")
 
 let nextDayDate = document.createElement("li")
 let nextDayTemp = document.createElement("li")
 let nextDayWind = document.createElement("li")
 let nextDayHumidity = document.createElement("li")
 // Figure out how to loop this so each day doesn't need variables
-nextDay1 = fiveDayListEl.append(nextDayDate)
-nextDay2 = fiveDayListEl.append(nextDayTemp)
-nextDay3 = fiveDayListEl.append(nextDayWind)
-nextDay4 = fiveDayListEl.append(nextDayHumidity)
+nextDay1 = dayOneIDEl.append(nextDayDate)
+nextDay2 = dayOneIDEl.append(nextDayTemp)
+nextDay3 = dayOneIDEl.append(nextDayWind)
+nextDay4 = dayOneIDEl.append(nextDayHumidity)
 
-nextDayDate.textContent = 
-nextDayTemp.textContent = 
-nextDayWind.textContent =
-nextDayHumidity.textContent =
+nextDayDate.textContent = "Day:"
+nextDayTemp.textContent = "Current Temp:"
+nextDayWind.textContent = "Current Wind:"
+nextDayHumidity.textContent = "Current Humidity:"
+
 
 
 const apiKey = "64205da5f805a7fb413caa37e89fd954"
@@ -59,10 +63,16 @@ function handleSubmitAction(e) {
 
 function fetchWeather (city) {
     const currentWeather = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=imperial`
-    fetch(currentWeather).then(function(response) {
+    fetch(currentWeather)
+    .then(function(response) {
         return response.json()
     }) .then(function(data) {
-        console.log(data)
+            console.log(data)
+            console.log(data.main.temp)
+            selectedCityTitle.textContent = data.name + " " + dayjs().format("M/d/YYYY") 
+            currentTempEL.textContent = "Current Temp: " + Math.round(data.main.temp) + "ºF"
+            currentWindEl.textContent = "Current Wind: " + data.wind.speed + " MPH"
+            currentHumidityEl.textContent = "Current Humidity: " + data.main.humidity + "%"
         const {lat,lon} = data.coord
         const weatherAPI = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}`
         fetch(weatherAPI).then(function(response) {
@@ -70,6 +80,7 @@ function fetchWeather (city) {
             return response.json()
         }).then(function(data) {
             console.log(data)
+            
         })
     })
 }
@@ -85,9 +96,8 @@ document.querySelector("#submitCityQuery").addEventListener("submit", handleSubm
 
 
 /* PseudoCode
-        TODO: Create event listener for form 
-            *Need input element in HTML
-            *Need eventlistener on button
+        #What do I currently give a shit about?
+            *API data for. Current Temp, Current Wind, Current Humidity
 
 
 */
